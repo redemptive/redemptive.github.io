86d9b236de141b5cd8475d572cba9785a45e5bda
redemptive <ewan_forbes@yahoo.co.uk>
2019-12-14 21:09:31 +0000

    Updates

---
+++ precache-manifest.06fb39bd3959cf0d395ae607b2e8752e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a0525ba5a39b4472959c4ba80b0086",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "b3edd61a0c0841ca7549",
+    "url": "/react-gh-pages/static/css/main.f5ab706c.chunk.css"
+  },
+  {
+    "revision": "5f23b8ea4d97394d15cd",
+    "url": "/react-gh-pages/static/js/2.6832b908.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-gh-pages/static/js/2.6832b908.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3edd61a0c0841ca7549",
+    "url": "/react-gh-pages/static/js/main.c4b1b20f.chunk.js"
+  },
+  {
+    "revision": "d263583e042ece2dbb94",
+    "url": "/react-gh-pages/static/js/runtime-main.472e5b64.js"
+  }
+]);
