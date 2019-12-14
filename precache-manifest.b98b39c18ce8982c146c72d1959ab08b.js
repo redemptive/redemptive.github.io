26ee812d75a3157f9ffc587e92fb8e0184adb1fc
redemptive <ewan_forbes@yahoo.co.uk>
2019-12-14 21:15:42 +0000

    Updates

---
+++ precache-manifest.b98b39c18ce8982c146c72d1959ab08b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714b6a2886930a7043941fe54fe16c15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c4bdd051a8b5323de26",
+    "url": "/static/css/main.f5ab706c.chunk.css"
+  },
+  {
+    "revision": "5f23b8ea4d97394d15cd",
+    "url": "/static/js/2.6832b908.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.6832b908.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c4bdd051a8b5323de26",
+    "url": "/static/js/main.678569c6.chunk.js"
+  },
+  {
+    "revision": "77d0d4323e732fca3a22",
+    "url": "/static/js/runtime-main.1d2472e4.js"
+  }
+]);
