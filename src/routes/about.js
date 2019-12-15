db476dbbeb8f1c6926000a3fcb7fdb4cabe79502
redemptive <ewan_forbes@yahoo.co.uk>
2019-12-15 12:31:29 +0000

    Added more content on about page around the pipeline

--- src/routes/about.js
+++ src/routes/about.js
@@ -33,14 +33,32 @@ function About() {
 						The react project gets built and deployed to GitHub pages.
 					</p>
 					<p>
-						This site is a react project. 
+						This site is made with the help of react. 
 						React-router-dom provides routing between different pages. 
 						I've chosen hashrouter specifically for this.
-						Routes are in their own folder in the project.
+						Routes are separated out to their own folder in the repository source.
 					</p>
 					<p>
 						Having already been very familiar with node js and react, it was a fun project.
 					</p>
+					<h3>The Pipeline</h3>
+					<p>
+						The pipeline I created using GitHub actions.
+						I recently stumbled onto this and immediately wanted to learn to use it.
+						Before, I had been using a Raspberry PI with jenkins installed.
+						It is great having such a CI tool available for free.
+					</p>
+					<p>
+						A good pipeline setup automates boring, time consuming manual tasks and eliminates the possibility of human error.
+						Here is what mine does:
+					</p>
+					<ul>
+						<li>Triggered on push to dev branch</li>
+						<li>Installs npm packages</li>
+						<li>Checks linting with eslint</li>
+						<li>Builds the react project</li>
+						<li>Deploys to the master branch which GitHub pages serves from</li>
+					</ul>
 				</section>
 			</div>
 		</div>
